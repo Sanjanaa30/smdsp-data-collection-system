808e371ec229766358e927770eac3a348a232fa9
Karthik MP <karthikmp521@gmail.com>
2025-10-14 15:01:56 -0400

    updating changes

--- 4chan_crawler/logger.py
+++ 4chan_crawler/logger.py
@@ -2,6 +2,7 @@ import logging
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
 
 load_dotenv(Path(__file__).resolve().parent.parent / ".env")
 
@@ -13,5 +14,15 @@ numeric_level = getattr(logging, log_level_str, logging.INFO)
 logger.setLevel(numeric_level)
 sh = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-sh.setFormatter(formatter)
-logger.addHandler(sh)
+
+log_mode = os.getenv("CHAN_LOG_MODE").upper()
+if log_mode == "FILE":
+    # Logging into files
+    log_file = os.getenv("CHAN_LOG_FILE", "4chan_crawler.log") 
+    fh = RotatingFileHandler(log_file, maxBytes=10_000_000, backupCount=5)  # 10MB max, 5 backups
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+else:
+    # Console logger 
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
--- reddit_crawler/logger.py
+++ reddit_crawler/logger.py
@@ -4,19 +4,22 @@ from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger("reddit_crawler")
 logger.propagate = False
-log_level_str = os.getenv("LOG_LEVEL", "INFO").upper()
+log_level_str = os.getenv("REDDIT_LOG_LEVEL").upper()
 numeric_level = getattr(logging, log_level_str, logging.INFO)
 
 logger.setLevel(numeric_level)
 sh = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-# Console logger 
-sh.setFormatter(formatter)
-logger.addHandler(sh)
+log_mode = os.getenv("REDDIT_LOG_MODE").upper()
+if log_mode == "FILE":
+    # Logging into files
+    log_file = os.getenv("LOG_FILE", "reddit_crawler.log") 
+    fh = RotatingFileHandler(log_file, maxBytes=10_000_000, backupCount=5)  # 10MB max, 5 backups
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+else:
+    # Console logger 
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
 
-# Logging into files
-# log_file = os.getenv("LOG_FILE", "reddit_crawler.log") 
-# fh = RotatingFileHandler(log_file, maxBytes=10_000_000, backupCount=5)  # 10MB max, 5 backups
-# fh.setFormatter(formatter)
-# logger.addHandler(fh)
